36f8e9c3743ab2299e55cc36a0a3a1c8569ac5de
nour-bassem <140580780+nour-bassem@users.noreply.github.com>
2023-11-23 19:04:06 +0200

    Update User.java
    user class implementation done

--- trials1/src/trials1/User.java
+++ trials1/src/trials1/User.java
@@ -1,12 +1,14 @@
+//updated
+
+package projectfiles;
+
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
-package trials1;
 
-
-package JavaApplication3;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -18,7 +20,6 @@ import java.io.BufferedWriter;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
-
 public class User 
 {
     
@@ -27,9 +28,10 @@ public class User
     public String userType;
     private String Password;
     private String confirmPass;
+       
     public String filepath= "userData.txt";
-
     public  ArrayList<User> userDataList = new ArrayList<>();
+    public  ArrayList<String> userLoginData = new ArrayList<>();
 
     
     public User(){}
@@ -40,6 +42,7 @@ public class User
         this.Password = Password;
     }
 
+    
     public String getUserName() 
     {
         return userName;
@@ -48,11 +51,10 @@ public class User
     public String getUserType() {
         return userType;
     }
-
-    public void setPassword(String Password){
+    public void setpassword(String Password){
         this.Password= Password;
     }
-    public String getPassword(){
+    public String getpassword(){
         return Password;
     }
     public void setconfirmPass(String confirmPass){
@@ -63,7 +65,7 @@ public class User
     }
    
     
-    //done 
+    
     public void createFile()
     {
            try
@@ -76,8 +78,7 @@ public class User
              }
              else 
              {
-                 System.out.println("File already exists");
-             
+                 System.out.println("File already exists");             
              }
          }
          catch(IOException e)
@@ -86,8 +87,58 @@ public class User
          }   
     }
     
+     public void signUp()
+     {
+          
+         Scanner input = new Scanner(System.in);
+         System.out.println("Enter your Usermane: ");
+         userName=input.next();
+         System.out.println("Register as: 1) Admin 2) Casheir 3) Customer ");
+         userType=input.next();
+         
+         System.out.println("Enter your password: ");
+         setpassword(input.next());
+         System.out.println("Confirm password: ");
+         setconfirmPass(input.next());
+
+         
+         if(getpassword().equals(getconfirmPass()))
+         { 
+         if(checkUserData(userName,userType,getpassword()))
+           {
+              System.out.println("Already registred");
+              return;
+          }
+             
+//          Scanner readFile = new Scanner("userData.txt");
+               
+        try (BufferedWriter writerToUserData = new BufferedWriter(new FileWriter("userData.txt", true))) 
+        {
+            // Write data to the file
+            writerToUserData.write(userName+" "+userType+" "+getpassword());
+            writerToUserData.newLine(); // Add a new line
+
+
+            System.out.println("egistred successfully!");
+        }
+        catch (IOException e) 
+        {
+            System.err.println("Error writing to file: " + e);
+        }  
+         }
+           
+        
+         else
+         {
+         System.out.println("Confirmation doesn't match password, try again ");
+         signUp(); 
+         }
     
-    public void readFromFile(String filepath)
+          }
+      
+         
+     
+      public void readFromFile(String filepath)
     {
          try (BufferedReader reader = new BufferedReader(new FileReader(filepath)))
          {
@@ -100,7 +151,7 @@ public class User
                     String userType1 = parts[1];
                     String password1= parts[2];
 
-                    // Create a new User object and add it to  userDataList
+                    // Create a new User object and add it to  userDataList(arraylist)
                     User newUser = new User(userName1, userType1, password1);
                     userDataList.add(newUser);
                 } else {
@@ -108,65 +159,36 @@ public class User
                 }
             }
 
-            System.out.println("User data read from file successfully.");
+            System.out.println("User data read from file into arraylist successfully.");
         } catch (IOException e) {
             System.out.println("Error reading from file: " + e);
         }
     }
 
+    private boolean checkUserData(String userName1,String userType1,String Password1) {
+         for(User i:userDataList){
+             if(i.userName.equals(userName1)  && i.userType.equals(userType1) && i.Password.equals(Password1) )
+             {
+                 return true;
+             }             
+         }
+         return false;
+    }   
     
-        
-    
-    
-    
-    
-    
-    public void writeToFile(){
-        
-       try (FileWriter writer = new FileWriter ("userData.txt"))
-       {
-           //looping through the  arraylist 
-        for (User u : userDataList) 
-        {
-            //writing its contents on a file
-            writer.write(u.getUserName()+" "+u.getUserType()+" "+u.getPassword()+"\n");
-            
-        }
-        System.out.println("User data saved to file successfully.");
-    } catch (IOException e) {
-       System.out.println(e);
-    }
-}
-        
-    
-    
-    
-    //done
-     public void signUp()
-     {
-            Scanner userInput2 = new Scanner(System.in);
-            System.out.println("Enter user name: ");
-            String userName1 = userInput2.next();
-            System.out.println("Enter user type: ");
-            String userType1 = userInput2.next();
-            System.out.println("Enter password: ");
-            String password1 = userInput2.next();
-            
-            //taking data from user
-            //creating an object and putting it in the array list 
+//     private boolean checkUserloginData( ArrayList<String> userLoginDat) {
+//         for(String data:userLoginDat){
+//             if(data.contentEquals(userName)  && data.contentEquals(userType) && data.contentEquals(Password))
+//             {
+//                 return true;
+//             }             
+//         }
+//         return false;
+//    }
+     
 
-          User u1  = new User(userName1, userType1, password1);
-          //adding object to arraylist
-          userDataList.add(u1);
-         
-     }
-
-    //private boolean checkUserData(String userName, String userType, String password) 
-    //{return 0;
-    //}
-
-
-    public void logIn()
+    
+     
+    public boolean logIn()
     {
     Scanner userInput2 = new Scanner(System.in);
     System.out.println("Enter user name: ");
@@ -175,18 +197,27 @@ public class User
     String userType1 = userInput2.next();
     System.out.println("Enter password: ");
     String password1 = userInput2.next();
-
-    //if (checkUserData(userName1, userType1, password1)) 
-    //{
-      //  System.out.println("Login successful!");
-    //} else 
-    //{
-      //  System.out.println("Login failed. Invalid credentials.");
-    //}
-
-    //}
-    }
-}
     
+//    User loginData=new User(userName1,userType1,password1);
 
+    return checkUserData(userName1,userType1,password1);
+    }
+    
+     public void writeToFile(){
 
+       try (FileWriter writer = new FileWriter ("userData.txt"))
+       {
+           //looping through the  arraylist 
+        for (User u : userDataList) 
+        {
+            //writing its contents on a file
+            writer.write(u.userName+" "+u.userType+" "+u.getpassword()+"\n");
+
+        }
+        System.out.println("User data saved to file successfully.");
+    } catch (IOException e) {
+       System.out.println(e);
+    }
+    }
+  
+}
